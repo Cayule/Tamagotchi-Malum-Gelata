52323264d9cdd0a3202bd181680fee088b9b6d82
Cayule <ayule6@gmail.com>
2021-11-30 18:15:24 -0800

    adding JS and CSS changes

--- JAVASCRIPT/script.js
+++ JAVASCRIPT/script.js
@@ -1,5 +1,35 @@
-function sanityCheck () {
-    console.log("We all float down here")
+// function sanityCheck () {
+//     console.log("We all float down here")
+// }
+
+// sanityCheck()
+
+// Steps:
+//Game should show title screen w/instructions
+//Once pet name is entered, game should start
+//Game screen should show pet name, 3 buttons that correspond to 3 bars, and show age
+//when pet hits a certain age, change it's graphic
+//when win/lose conditions are met, players should see a result screen
+
+// Action buttons
+const $startGame = $('.start-game');
+
+
+
+//Hide Title Screen
+function hideAll() {
+    $('.Title-Screen').css('display', 'none');   
 }
 
-sanityCheck()
+//Start Button Test
+$( ".start-button" ).click(function(event) {
+//   alert( "Handler for .click() called." );
+event.preventDefault()
+hideAll()
+});
+
+function onClick () {
+
+}
+
+
